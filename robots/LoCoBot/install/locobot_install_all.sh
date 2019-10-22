a07af8bde1fef426503f91250c2d24634068f86e
kalyanvasudev <kalyan051993@gmail.com>
2019-10-22 14:00:31 -0400

    debug7

--- robots/LoCoBot/install/locobot_install_all.sh
+++ robots/LoCoBot/install/locobot_install_all.sh
@@ -192,8 +192,8 @@ cd $LOCOBOT_FOLDER
 rosdep update 
 rosdep install --from-paths src -i -y
 cd $LOCOBOT_FOLDER/src/pyrobot/robots/LoCoBot/install
-chmod +x install_orb_slam2.sh
-source install_orb_slam2.sh
+#chmod +x install_orb_slam2.sh
+#source install_orb_slam2.sh
 cd $LOCOBOT_FOLDER
 if [ -d "$LOCOBOT_FOLDER/devel" ]; then
 	rm -rf $LOCOBOT_FOLDER/devel
