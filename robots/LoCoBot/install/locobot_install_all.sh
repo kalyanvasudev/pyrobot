b7d3c2095cad944a43fbf8edd572c4b14897c4ca
kalyanvasudev <kalyan051993@gmail.com>
2019-10-23 16:14:28 -0400

    debug 8

--- robots/LoCoBot/install/locobot_install_all.sh
+++ robots/LoCoBot/install/locobot_install_all.sh
@@ -45,17 +45,17 @@ echo -e "\e[1;33m ******************************************* \e[0m"
 sleep 4
 start_time="$(date -u +%s)"
 
-install_packages () {
-	pkg_names=("$@")
-	for package_name in "${pkg_names[@]}"; 
-	do
-		if [ $(dpkg-query -W -f='${Status}' $package_name 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-		    sudo apt-get -y install $package_name
-		else
-		    echo "${package_name} is already installed";
-		fi
-	done
-}
+# install_packages () {
+# 	pkg_names=("$@")
+# 	for package_name in "${pkg_names[@]}"; 
+# 	do
+# 		if [ $(dpkg-query -W -f='${Status}' $package_name 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+# 		    sudo apt-get -y install $package_name
+# 		else
+# 		    echo "${package_name} is already installed";
+# 		fi
+# 	done
+# }
 
 
 # STEP 0 - Make sure you have installed Ubuntu 16.04, and upgrade to lastest dist
